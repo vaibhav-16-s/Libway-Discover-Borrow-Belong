d560c2dee721a097a08b86dc7201d5964d272c26
Vaibhav Singh <you@example.com>
2026-07-04 08:58:07 +0530

    Created folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import {browserRouter as Route,Router,Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/mycomponent/home/Home.jsx
@@ -0,0 +1,7 @@
+export const Home=()=>{
+    return(
+        <>
+            <h3>This is Landing Page</h3>
+        </>
+    );
+}
